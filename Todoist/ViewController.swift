56b0565ecca506716abf8ab711ac64f25086af01
Rostislav Mikhal <elliotmillerz66@gmail.com>
2021-12-11 00:07:28 +0200

    no message

--- Todoist/ViewController.swift
+++ Todoist/ViewController.swift
@@ -8,7 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var greetLabel: UILabel!
+    @IBOutlet weak var backImage: UIImageView!
+    
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var noAccGreetLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registrationButton: UIButton!
+    
+    // test login things
+    let exceptedEmail: String = "test@gmail.com"
+    let exceptedPass: String = "12345678"
+    
+    
+    //login into account
+    @IBAction func loginAction(_ sender: Any) {
+        
+        if let email = loginField.text,
+           let password = passwordField.text{
 
+            if email.contains("@"){
+                if password.count > 8{
+                    if email == exceptedEmail{
+                        if password == exceptedPass{
+                            
+                        }else{print("Wrong password")}
+                    
+                    }else{print("Wrong mail")}
+            }else{print("Wrong password")}
+            }else{print("Must contain '@' in login field")}
+
+
+    
+    
+}
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
